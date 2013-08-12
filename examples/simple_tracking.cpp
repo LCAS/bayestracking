933b7e4441c2d8e8c7532fe85cc24221af94bacf
Marc Hanheide <marc@hanheide.net>
2013-08-12 14:07:05 +0100

    initial import form package

---
+++ examples/simple_tracking.cpp
@@ -0,0 +1,248 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Nicola Bellotto                                 *
+ *   nbellotto@lincoln.ac.uk                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "multitracker.h"
+#include "models.h"
+#include "ekfilter.h"
+#include "ukfilter.h"
+#include "pfilter.h"
+#include <sys/time.h>
+#include "trackwin.h"
+#include <cstdio>
+
+#define SLEEP_TIME 10000  // sleep time in [us]
+
+
+using namespace std;
+using namespace MTRK;
+using namespace Models;
+
+
+typedef PFilter Filter;
+const int numOfTarget = 3;
+const association_t alg = NNJPDA;
+
+static double getTime()
+{
+  timeval t;
+  gettimeofday(&t,NULL);
+  return (double)t.tv_sec + (double)t.tv_usec/1000000.0;
+}
+
+
+// rule to detect lost track
+template<class FilterType>
+bool MTRK::isLost(const FilterType* filter) {
+  // track lost if var(x)+var(y) > 1
+  if (filter->X(0,0) + filter->X(2,2) > sqr(1.0))
+    return true;
+  return false;
+}
+
+// rule to create new track
+template<class FilterType>
+bool MTRK::initialize(FilterType* &filter, sequence_t& obsvSeq) {
+  assert(obsvSeq.size());
+
+  double dt = obsvSeq.back().time - obsvSeq.front().time;
+  assert(dt); // dt must not be null
+  FM::Vec v((obsvSeq.back().vec - obsvSeq.front().vec) / dt);
+  
+  FM::Vec x(4);
+  FM::SymMatrix X(4,4);
+  
+  x[0] = obsvSeq.back().vec[0];
+  x[1] = v[0];
+  x[2] = obsvSeq.back().vec[1];
+  x[3] = v[1];
+  X.clear();
+  X(0,0) = sqr(0.5);
+  X(1,1) = sqr(1.5);
+  X(2,2) = sqr(0.5);
+  X(3,3) = sqr(1.5);
+
+  filter = new FilterType(4);
+  filter->init(x, X);
+
+  return true;
+}
+
+
+// command line option
+bool textDebug = false;
+bool winDebug = false;
+bool sendPredictions = false;
+
+
+int main(int argc, char *argv[]) {
+  cout.precision(20);
+
+  // parse arguments
+  for (int c = 1; c < argc; c++) {
+    if (strcmp(argv[c], "-g") == 0 ) {
+      textDebug = true;
+      continue;
+    }
+    if (strcmp(argv[c], "-w") == 0 ) {
+      winDebug = true;
+      continue;
+    }
+    cerr << "Unknown option " << argv[c] << endl;
+    cerr << "Valid options are '-g' (text debug) and '-w' (visual debug)" << endl;
+    exit(EXIT_FAILURE);
+  }
+  
+  MultiTracker<Filter, 4> mtrk;    // state [x, v_x, y, v_y]
+  CVModel cvm(5.0, 5.0);           // CV model with sigma_x = sigma_y = 5.0
+  CartesianModel ctm(0.5, 0.5);              // Cartesian observation model
+  FM::Vec observation(2);          // observation [x, y]
+  vector<FM::Vec> obsvBuffer;      // observation buffer
+
+  double dt, time = getTime(), obsvTime = -1;
+
+  TrackWin* trkwin = 0;
+  if (winDebug) {
+    trkwin = new TrackWin(__FILE__, 640, 480, 20.);
+    trkwin->setOrigin(0., 0., 0.);
+    trkwin->create();
+  }
+  char label[256];
+
+#if 0
+  Filter filter(4);
+  static bool initialized = false;
+  for (;;) {
+    dt = getTime() - time;
+    time += dt;
+
+    if (!initialized) {
+      FM::Vec x(4);
+      x[0] = 2 * cos(time);
+      x[1] = 0;
+      x[2] = 2 * sin(time);
+      x[3] = 0;
+      FM::SymMatrix X(4,4);
+      X.clear();
+      X(0,0) = sqr(0.5);
+      X(1,1) = sqr(0.1);
+      X(2,2) = sqr(0.5);
+      X(3,3) = sqr(0.1);
+      filter.init(x, X);
+      initialized = true;
+      cout << "initialized" << endl;
+    }
+
+    // prediction
+    cvm.update(dt);
+    filter.predict(cvm);
+
+    // observation
+    if (time - obsvTime > 30e-3) {
+      obsvTime = time;
+      FM::Vec obsv(2);
+      obsv[0] = 2 * cos(time);
+      obsv[1] = 2 * sin(time);
+      trkwin->setObject("O", obsv[0], obsv[1], TrackWin::NO_ORIENTATION, TrackWin::GREY);
+      filter.observe(bgm, obsv);
+    }
+
+    trkwin->setObject("T", filter.x[0], filter.x[2], atan2(filter.x[3], filter.x[1])/*orientation*/,
+          TrackWin::RED, sqrt(filter.X(0,0)), sqrt(filter.X(2,2))/*std dev*/);
+    trkwin->update(5);
+    
+    usleep(SLEEP_TIME);
+  }
+
+#else
+  for (;;) {
+    ///////////////// estimation begin /////////////////
+//     #warning fix 'dt' if not debugging
+    dt = /*20e-3;*/getTime() - time;
+    time += dt;
+
+    // prediction
+    cvm.update(dt);
+    mtrk.predict<CVModel>(cvm);
+
+    // observation
+    obsvBuffer.clear();
+    if (time - obsvTime > 30e-3) {
+      obsvTime = time;
+      for (int i = 0; i < numOfTarget; i++) {
+        FM::Vec obsv(2);
+        // counter-clockwise 
+        obsv[0] = 2 * (i+1) * cos(time / (i+1));
+        obsv[1] = 2 * (i+1) * sin(time / (i+1));
+        obsvBuffer.push_back(obsv);
+        // clockwise
+        obsv[0] = 2 * (i+1) * cos(-time / (i+1));
+        obsv[1] = 2 * (i+1) * sin(-time / (i+1));
+        obsvBuffer.push_back(obsv);
+        // put some false positives
+        if (rand() > 0.95*RAND_MAX) {
+          obsv[0] *= 2.0 * rand() / RAND_MAX;
+          obsv[1] *= 3.0 * rand() / RAND_MAX;
+          obsvBuffer.push_back(obsv);
+        }
+      }
+
+      // add last observation/s to tracker
+      vector<FM::Vec>::iterator li, liEnd = obsvBuffer.end();
+      for (li = obsvBuffer.begin(); li != liEnd; li++) {
+        observation[0] = (*li)[0];
+        observation[1] = (*li)[1];
+        mtrk.addObservation(observation, obsvTime);
+
+        if (winDebug) {
+          // show on trackwin
+          sprintf(label, "(%f,%f)", observation[0], observation[1]);
+          trkwin->setObject(label, observation[0], observation[1], TrackWin::NO_ORIENTATION, TrackWin::GREY);
+        }
+      }
+      
+      // process observations (if available) and update tracks
+      mtrk.process(ctm, alg);
+//       mtrk.print();
+    }
+    ///////////////// estimation end /////////////////
+
+    // show on trackwin
+    if (winDebug) {
+    int n = mtrk.size();
+      for (int i = 0; i < n; i++) {
+        sprintf(label, "trk_%ld", mtrk[i].id);
+        trkwin->setObject(label, mtrk[i].filter->x[0], mtrk[i].filter->x[2], atan2(mtrk[i].filter->x[3], mtrk[i].filter->x[1])/*orientation*/,
+          TrackWin::RED, sqrt(mtrk[i].filter->X(0,0)), sqrt(mtrk[i].filter->X(2,2))/*std dev*/);
+      }
+      trkwin->update(5);
+    }
+
+    int slp = SLEEP_TIME - (int)(1e6 * (getTime() - time));
+    usleep(slp > 0 ? slp : 0);
+  }
+#endif
+
+  if (winDebug) {
+    trkwin->destroy();
+    delete trkwin;
+  }
+
+return EXIT_SUCCESS;
+}
---
+++ src/models.cpp
@@ -0,0 +1,240 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Nicola Bellotto                                 *
+ *   nbellotto@lincoln.ac.uk                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "models.h"
+#include "BayesFilter/matSup.hpp"
+// #include <angle.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <float.h>
+
+
+using namespace Bayesian_filter;
+using namespace Models;
+
+
+//*************************************************************************
+//                      CV PREDICTION MODEL
+//*************************************************************************
+
+CVModel::CVModel(Float wxSD, Float wySD) :
+   Linrz_predict_model(x_size, q_size),
+   Sampled_predict_model(),
+   fx(x_size),
+   genn(rnd),
+   xp(x_size),
+   n(q_size), rootq(q_size),
+   m_wxSD(wxSD), m_wySD(wySD)
+{
+   first_init = true;
+   init();
+}
+
+
+void CVModel::init()
+{
+  Fx.clear();
+  // x
+  Fx(0,0) = 1.;
+  Fx(0,1) = dt;
+  Fx(0,2) = 0.;
+  Fx(0,3) = 0.;
+  // dx
+  Fx(1,0) = 0.;
+  Fx(1,1) = 1.;
+  Fx(1,2) = 0.;
+  Fx(1,3) = 0.;
+  // y
+  Fx(2,0) = 0.;
+  Fx(2,1) = 0.;
+  Fx(2,2) = 1.;
+  Fx(2,3) = dt;
+  // dy
+  Fx(3,0) = 0.;
+  Fx(3,1) = 0.;
+  Fx(3,2) = 0.;
+  Fx(3,3) = 1.;
+  // noise
+  q[0] = sqr(m_wxSD); // cov(w_x)
+  q[1] = sqr(m_wySD); // cov(w_y)
+  G.clear();
+  G(0,0) = 0.5*sqr(dt);
+  G(1,0) = dt;
+  G(2,1) = 0.5*sqr(dt);
+  G(3,1) = dt;
+}
+
+
+const FM::Vec& CVModel::f(const FM::Vec& x) const
+{  // human model
+   fx[0] = x[0] + x[1] * dt;  // x
+   fx[1] = x[1];              // dx
+   fx[2] = x[2] + x[3] * dt;  // y
+   fx[3] = x[3];              // dy
+   return fx;
+}
+
+
+void CVModel::update(double dt)
+{
+  // time interval
+  this->dt = dt;
+
+  //     | 0.5dt^2     0    |
+  // G = |    dt       0    |
+  //     |    0     0.5dt^2 |
+  //     |    0        dt   |
+  G(0,0) = 0.5*sqr(dt);
+  G(1,0) = dt;
+  G(2,1) = 0.5*sqr(dt);
+  G(3,1) = dt;
+}
+
+
+void CVModel::updateJacobian(const FM::Vec& x) {
+   //      | 1  dt 0  0 |
+   // Fx = | 0  1  0  0 |
+   //      | 0  0  1  dt|
+   //      | 0  0  0  1 |
+  Fx(0,1) = dt;
+  Fx(2,3) = dt;
+}
+
+
+const Vec& CVModel::fw(const FM::Vec& x) const
+/*
+   * Definition of sampler for addative noise model given state x
+   *  Generate Gaussian correlated samples
+   * Precond: init_GqG, automatic on first use
+   */
+{
+   if (first_init)
+      init_GqG();
+                  // Predict state using supplied functional predict model
+   xp = f(x);
+                  // Additive random noise
+   CVModel::genn.normal(n);            // independant zero mean normal
+                        // multiply elements by std dev
+   for (FM::DenseVec::iterator ni = n.begin(); ni != n.end(); ++ni) {
+      *ni *= rootq[ni.index()];
+   }
+   FM::noalias(xp) += FM::prod(this->G,n);         // add correlated noise
+   return xp;
+}
+
+
+void CVModel::init_GqG() const
+/* initialise predict given a change to q,G
+   *  Implementation: Update rootq
+   */
+{
+   first_init = false;
+   for (FM::Vec::const_iterator qi = this->q.begin(); qi != this->q.end(); ++qi) {
+      if (*qi < 0)
+         error (Numeric_exception("Negative q in init_GqG"));
+      rootq[qi.index()] = std::sqrt(*qi);
+   }
+}
+
+
+
+//*************************************************************************
+//                   CARTESIAN SUBTRACTION OBSERVATION MODEL
+//*************************************************************************
+
+CartesianModel::CartesianModel(Float xSD, Float ySD) :
+   Linrz_correlated_observe_model(x_size, z_size),
+   Likelihood_observe_model(z_size),
+   z_pred(z_size),
+   li(z_size)
+{
+  // Hx = | 1  0  0  0 |
+  //      | 0  0  1  0 |
+  Hx.clear();
+  Hx(0,0) = 1.;
+  Hx(1,2) = 1.;
+  // noise
+  Z.clear();
+  Z(0,0) = sqr(xSD);
+  Z(1,1) = sqr(ySD);
+}
+
+Bayes_base::Float
+ CartesianModel::Likelihood_correlated::L(const Correlated_addative_observe_model& model, const FM::Vec& z, const FM::Vec& zp) const
+/*
+ * Definition of likelihood given an addative Gaussian observation model:
+ *  p(z|x) = exp(-0.5*(z-h(x))'*inv(Z)*(z-h(x))) / sqrt(2pi^nz*det(Z));
+ *  L(x) the the Likelihood L(x) doesn't depend on / sqrt(2pi^nz) for constant z size
+ * Precond: Observation Information: z,Z_inv,detZterm
+ */
+{
+   if (!zset)
+      Bayes_base::error (Logic_exception ("BGSubModel used without Lz set"));
+               // Normalised innovation
+   zInnov = z;
+   model.normalise (zInnov, zp);
+   FM::noalias(zInnov) -= zp;
+
+   Float logL = scaled_vector_square(zInnov, Z_inv);
+   using namespace std;
+   return exp(Float(-0.5)*(logL + z.size()*log(2*M_PI) + logdetZ));   // normalized likelihood
+}
+
+
+void CartesianModel::Likelihood_correlated::Lz (const Correlated_addative_observe_model& model)
+/* Set the observation zz and Z about which to evaluate the Likelihood function
+ * Postcond: Observation Information: z,Z_inv,detZterm
+ */
+{
+   zset = true;
+                  // Compute inverse of Z and its reciprocal condition number
+   Float detZ;
+   Float rcond = FM::UdUinversePD (Z_inv, detZ, model.Z);
+   model.rclimit.check_PD(rcond, "Z not PD in observe");
+                  // detZ > 0 as Z PD
+   using namespace std;
+   logdetZ = log(detZ);
+}
+
+
+Bayes_base::Float
+ CartesianModel::Likelihood_correlated::scaled_vector_square(const FM::Vec& v, const FM::SymMatrix& V)
+/*
+ * Compute covariance scaled square inner product of a Vector: v'*V*v
+ */
+{
+   return FM::inner_prod(v, FM::prod(V,v));
+}
+
+
+const FM::Vec& CartesianModel::h(const FM::Vec& x) const
+{
+  z_pred[0] = x[0];
+  z_pred[1] = x[2];
+  return z_pred;
+};
+   
+
+void CartesianModel::updateJacobian(const FM::Vec& x) {
+  // nothing to do
+}
+
+
+void CartesianModel::normalise(FM::Vec& z_denorm, const FM::Vec& z_from) const {
+}
