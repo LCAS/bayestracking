65153b9994ae9b0ed1a7ba3e2ff64d95637815c1
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-08-11 14:34:56 +0100

    Refactored bayestracking library to be a catkin package Created a cleaner file structure by seperating the src from the include files.

---
+++ include/bayestracking/BayesFilter/allFilters.hpp
@@ -0,0 +1,28 @@
+#ifndef _ALL_FILTERS
+#define _ALL_FILTERS
+
+/*
+ * Bayes++ the Bayesian Filtering Library
+ * Copyright (c) 2002 Michael Stevens
+ * See accompanying Bayes++.htm for terms and conditions of use.
+ *
+ * $Id$
+ */
+
+/*
+ * Bayesian Filters.
+ *  A Bayesian filters as an Abstract class.
+ *  All filters have similar structure with vary algorithms and numerical properties
+ */
+
+#include "bayestracking/BayesFilter/UDFlt.hpp"
+#include "bayestracking/BayesFilter/CIFlt.hpp"
+#include "bayestracking/BayesFilter/unsFlt.hpp"
+#include "bayestracking/BayesFilter/covFlt.hpp"
+#include "bayestracking/BayesFilter/infFlt.hpp"
+#include "bayestracking/BayesFilter/infRtFlt.hpp"
+#include "bayestracking/BayesFilter/itrFlt.hpp"
+#include "bayestracking/BayesFilter/SIRFlt.hpp"
+#include "bayestracking/BayesFilter/models.hpp"
+
+#endif
