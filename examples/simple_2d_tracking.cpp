65912bbbda22383907efaf736433e97a834dbcd0
scosar <scosar@lincoln.ac.uk>
2016-04-25 10:18:26 +0100

    Added option to specify minimum number of observations, minimum interval between observations for new track creation and a variance limit for track deletion

--- examples/simple_2d_tracking.cpp
+++ examples/simple_2d_tracking.cpp
@@ -49,9 +49,9 @@ static double getTime()
 
 // rule to detect lost track
 template<class FilterType>
-bool MTRK::isLost(const FilterType* filter) {
-  // track lost if var(x)+var(y) > 1
-  if (filter->X(0,0) + filter->X(2,2) > sqr(1.0))
+bool MTRK::isLost(const FilterType* filter, double varLimit = 1.0) {
+    // track lost if var(x)+var(y) > varLimit
+    if (filter->X(0,0) + filter->X(2,2) > sqr(varLimit))
     return true;
   return false;
 }
